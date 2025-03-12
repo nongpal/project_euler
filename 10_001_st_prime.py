db8bfa2aefd494b89f46be4d845ccd286dcfde60
nongpal <143192751+nongpal@users.noreply.github.com>
2025-03-12 22:07:32 +0700

    Create 10_001_st_prime.py

---
+++ 10_001_st_prime.py
@@ -0,0 +1,12 @@
+def main(limits: int = 10001):
+    result = [2]
+    attempt = 3
+    while len(result) < limits:
+        if all(attempt % prime != 0 for prime in result):
+            result.append(attempt)
+        attempt += 2
+    return result[-1]
+
+if __name__ == "__main__":
+    result = main()
+    print(f"Result: {result}")
